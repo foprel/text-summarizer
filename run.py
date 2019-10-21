037d4044286660646da8cfd734261558daece6a0
foprel <oprel.fj@gmail.com>
2019-10-21 19:52:56 +0200

    added examples folder and added Jupyter Notebook to .gitignore

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import Smmry
 import io
 
-text = io.open('article_1.txt', 'r', encoding='utf-8').read()
+text = io.open('examples/article_1.txt', 'r', encoding='utf-8').read()
 smmry = Smmry(text, lang="english")
 smmry.summarize(length=2)
